c9b2793b5260ed261ebc1f2e4594199b9bd4ce24
fan3cn <fan3cn@outlook.com>
2018-11-14 00:01:18 -0500

    add OHLC log

--- bitmex_bot/utils/log.py
+++ bitmex_bot/utils/log.py
@@ -7,8 +7,26 @@ def setup_custom_logger(name, log_level=settings.LOG_LEVEL):
 
     handler = logging.StreamHandler()
     handler.setFormatter(formatter)
+    logger = logging.getLogger(name)
+    logger.setLevel(log_level)
+    logger.addHandler(handler)
+
+    fh = logging.FileHandler('log/info.log')
+    fh.setLevel(log_level)
+    fh.setFormatter(formatter)
+    logger.addHandler(fh)
+
+    return logger
+
+def setup_OHLC_logger(name, log_level=settings.LOG_LEVEL):
+    formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
+
+    handler = logging.FileHandler('log/OHLC.log')
+    handler.setFormatter(formatter)
 
     logger = logging.getLogger(name)
     logger.setLevel(log_level)
     logger.addHandler(handler)
     return logger
+
+
